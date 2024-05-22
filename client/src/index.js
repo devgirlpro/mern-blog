af450173be1eda5ad1e91d1791d582db00dce336
devgirlpro <agalidari@gmail.com>
2024-05-22 14:23:03 +0200

    added pages & dynamic routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
+import PostDetals from './pages/PostDetails';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import UserProfile from './pages/UserProfile';
+import Authors from './pages/Authors';
+import CreatePost from './pages/CreatePost';
+import CategoryPosts from './pages/CategoryPosts';
+import AuthorPosts from './pages/AuthorPosts';
+import Dashboard from './pages/Dashboard';
+import EditPost from './pages/EditPost';
+import Logout from './pages/Logout';
 
-import App from './App';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'post/:id',
+        element: <PostDetals />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'profile/:id',
+        element: <UserProfile />,
+      },
+      {
+        path: 'authors',
+        element: <Authors />,
+      },
+      {
+        path: 'create',
+        element: <CreatePost />,
+      },
+      {
+        path: 'post/categories/:category',
+        element: <CategoryPosts />,
+      },
+      {
+        path: 'posts/users/:id',
+        element: <AuthorPosts />,
+      },
+      {
+        path: 'myposts/:id',
+        element: <Dashboard/>,
+      },
+      {
+        path: 'posts/:id/edit',
+        element: <EditPost />,
+      },
+      {
+        path: 'logout',
+        element: <Logout />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
